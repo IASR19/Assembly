e0671d2252ed868976b98ca5979e86fcbe77c67a
Itamar Ribeiro <itamar.asr19@gmail.com>
2023-03-24 14:25:08 -0300

    Aula 24-03

---
+++ Aula-Lab-24-03.asm
@@ -0,0 +1,23 @@
+.text
+
+li $t1, 2
+li $t2, 5
+add $t3,$t1,$t2
+
+li $t4, 89
+li $t5, 20
+add $t6,$t4,$t5
+
+li $t7, -80
+li $s0, 1234
+add $s1,$t7,$s0
+
+li $s2, 70
+li $s3, 24
+sub $s4,$s2,$s3
+
+li $s5, 20
+subi $s6, $s5, 150
+
+
+
