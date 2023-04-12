2f497c3a2aed5e8b41026e207981e710c8289250
Itamar Ribeiro <itamar.asr19@gmail.com>
2023-04-12 19:05:41 -0300

    aula 12-04 (add user integration)

--- pilha-mips.asm
+++ pilha-mips.asm
@@ -9,15 +9,36 @@ main:
     # inicializa o ponteiro de quadro
     addi $fp, $sp, 1000
 
-    # inserir um valor na pilha
-    li $t0, 10    # valor a ser inserido na pilha
-    addi $sp, $sp, -4    # decrementa o ponteiro de pilha
-    sw $t0, ($sp)    # armazena o valor na pilha
+    # insere valores na pilha
+    li $t0, 0    # inicializa o índice do topo da pilha
+    li $t2, 1    # define o número máximo de valores para inserir na pilha
+    addi $t3, $zero, 10    # define o valor padrão a ser inserido na pilha
+    loop:
+        # solicita entrada do usuário
+        li $v0, 5    # código da syscall read_int
+        syscall
+        # armazena o valor na pilha
+        addi $sp, $sp, -4    # decrementa o ponteiro de pilha
+        sw $v0, ($sp)    # armazena o valor na pilha
+        addi $t0, $t0, 1    # incrementa o índice do topo da pilha
+        # verifica se atingiu o limite de valores para inserir
+        bne $t0, $t2, loop    # volta ao início do loop se ainda não atingiu o limite
+        # caso contrário, pula para o próximo passo
 
-    # remover um valor da pilha
-    lw $t1, ($sp)    # carrega o valor do topo da pilha
-    addi $sp, $sp, 4    # incrementa o ponteiro de pilha
-    # agora $t1 contém o valor removido da pilha
+    # remove valores da pilha
+    li $t1, 0    # inicializa o índice do topo da pilha
+    loop2:
+        # remove o valor do topo da pilha
+        lw $t4, ($sp)    # carrega o valor do topo da pilha
+        addi $sp, $sp, 4    # incrementa o ponteiro de pilha
+        # exibe o valor removido da pilha
+        li $v0, 1    # código da syscall print_int
+        move $a0, $t4    # move o valor removido para $a0
+        syscall
+        addi $t1, $t1, 1    # incrementa o índice do topo da pilha
+        # verifica se todos os valores foram removidos
+        bne $t1, $t2, loop2    # volta ao início do loop se ainda não foram removidos
+        # caso contrário, pula para o próximo passo
 
     # saída do programa
     li $v0, 10    # código da syscall exit
